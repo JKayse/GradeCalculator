74ca9e1b9c9f30cfb6145a2c2b8c4a2437de5be3
JKayse <jkayse@smu.edu>
2014-04-26 01:20:35 -0500

    Added a use statement in the sql file.

--- GradeCalculator.sql
+++ GradeCalculator.sql
@@ -3,18 +3,26 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 26, 2014 at 04:52 AM
+-- Generation Time: Apr 26, 2014 at 06:19 AM
 -- Server version: 5.6.12-log
 -- PHP Version: 5.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
 -- Database: `gradecalculator`
 --
 CREATE DATABASE IF NOT EXISTS `gradecalculator` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `gradecalculator`;
+
+USE DATABASE gradecalculator;
 
 -- --------------------------------------------------------
 
@@ -24,12 +32,10 @@ USE `gradecalculator`;
 
 CREATE TABLE IF NOT EXISTS `category` (
   `categoryId` int(11) NOT NULL AUTO_INCREMENT,
+  `percentage` int(11) NOT NULL,
   `classId` int(11) NOT NULL,
   `categoryName` varchar(60) NOT NULL,
-  `percentage` int(11) NOT NULL,
-  PRIMARY KEY (`categoryId`),
-  UNIQUE KEY `classId` (`classId`),
-  UNIQUE KEY `categoryName` (`categoryName`)
+  PRIMARY KEY (`categoryId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -74,3 +80,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `userId` (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
